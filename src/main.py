634eeccea146b374c22b36f2ff95e8731a9956cd
stsaria <solothunder.autoer@gmail.com>
2024-08-03 19:39:55 +0900

    flaskの立ち上げを127.0.0.1から0.0.0.0に変更（一時的かも）

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
     if sys.argv[-1].isdecimal():
         port = int(sys.argv[-1])
     webbrowser.open(f"http://127.0.0.1:{port}")
-    endpoints.app.run(host="127.0.0.1", port=port)
+    endpoints.app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     main()
